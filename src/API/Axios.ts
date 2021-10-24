2098e3cd8a62ee7f9a0db782ab1d014b66586949
Jhelison Uchoa <68653689+jhelison@users.noreply.github.com>
2021-10-24 12:31:45 -0300

    Added the authorization

--- src/API/Axios.ts
+++ src/API/Axios.ts
@@ -4,4 +4,8 @@ const api = axios.create({
     baseURL: 'http://192.168.5.137:5151'
 })
 
+export const setToken = (token: string) => {
+    api.defaults.headers.common = { Authorization: `Bearer ${token}` }
+}
+
 export default api
