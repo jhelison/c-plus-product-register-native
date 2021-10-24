a6bf68ed7454914c0d2e3fd527045408b929824c
Jhelison Uchoa <68653689+jhelison@users.noreply.github.com>
2021-10-24 19:00:31 -0300

    Changed ip

--- src/API/Axios.ts
+++ src/API/Axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://192.168.5.137:5151'
+    baseURL: 'http://10.0.0.103:5151'
 })
 
 export const setToken = (token: string) => {
